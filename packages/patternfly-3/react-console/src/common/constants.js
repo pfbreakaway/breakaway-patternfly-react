ed4165b7aa8843f7697a851e0312fe1d1f0632c8
Dana Gutride <dgutride@redhat.com>
2018-11-26 11:39:30 -0500

    fix(build): tighten up build and remove patternfly-3

--- packages/patternfly-3/react-console/src/common/constants.js
+++
@@ -1,4 +0,0 @@
-export const NONE_TYPE = '_none_';
-export const SERIAL_CONSOLE_TYPE = 'SerialConsole';
-export const SPICE_CONSOLE_TYPE = 'SpiceConsole';
-export const VNC_CONSOLE_TYPE = 'VncConsole';
--- packages/patternfly-3/react-console/src/index.js
+++
@@ -1,7 +0,0 @@
-export * from './common/helpers';
-export * from './common/constants';
-
-export * from './SerialConsole';
-export * from './VncConsole';
-export * from './AccessConsoles';
-export * from './DesktopViewer';
