fc0ecdf816d02f261a791cb588fb3eab18d7cf40
Joachim Schuler <jschuler@redhat.com>
2018-11-26 14:51:50 -0500

    fix(core): alert label

--- packages/patternfly-4/react-core/src/components/Alert/Alert.js
+++ packages/patternfly-4/react-core/src/components/Alert/Alert.js
@@ -38,7 +38,7 @@ const defaultProps = {
   className: ''
 };
 
-const getDefaultAriaLabel = variant => `${capitalize(AlertVariant[variant])} Notification`;
+const getDefaultAriaLabel = variant => `${capitalize(AlertVariant[variant])} Notification - Test`;
 
 const Alert = ({
   variant,
