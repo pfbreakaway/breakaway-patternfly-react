b67b8def10b8209435de90fe93615671317a464b
Joachim Schuler <jschuler@redhat.com>
2018-12-04 16:23:00 -0500

    chore: export auth token and cleanup

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -7,7 +7,5 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 fi
 
 if [ "${TRAVIS_REPO_SLUG}" == "${TRIGGER_REPO_SLUG}" -a "$TRAVIS_BRANCH" == "${TRIGGER_REPO_BRANCH}" ]; then
-  # note: yarn travis-deploy-once "yarn lerna:publish" currently breaks NPM_TOKEN auth context
-  export GITHUB_AUTH="${GH_TOKEN}"
   npm run travis-deploy-once "npm run release"
 fi
