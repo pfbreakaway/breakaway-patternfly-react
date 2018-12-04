1d91173c86f5ed30fe089f441adee78191aa9b0a
Joachim Schuler <jschuler@redhat.com>
2018-12-04 17:53:07 -0500

    chore: debugging

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -7,5 +7,19 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 fi
 
 if [ "${TRAVIS_REPO_SLUG}" == "${TRIGGER_REPO_SLUG}" -a "$TRAVIS_BRANCH" == "${TRIGGER_REPO_BRANCH}" ]; then
+  # note: yarn travis-deploy-once "yarn lerna:publish" currently breaks NPM_TOKEN auth context
+  export GITHUB_AUTH="${GH_TOKEN}"
   npm run travis-deploy-once "npm run release"
+  echo "pwd"
+  pwd
+  npm run lerna:changelog
+  echo "ls -al"
+  ls -al
+  git add CHANGELOG.md
+  echo "git branch"
+  git branch
+  echo "git status"
+  git status
+  git commit -m 'chore(changelog): update changelog [skip ci]'
+  git push
 fi
