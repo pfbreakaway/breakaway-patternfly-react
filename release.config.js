ad8755875ed145b2ffe21121d492fa450f60d672
Joachim Schuler <jschuler@redhat.com>
2018-12-07 16:17:00 -0500

    chore: successComment [skip ci]

--- release.config.js
+++ release.config.js
@@ -1,9 +1,11 @@
+const PACKAGE_NAME = process.env.LERNA_PACKAGE_NAME || process.env.npm_package_name;
 const HOME_URL = 'https://github.com/semantic-release/semantic-release';
 const linkify = releaseInfo =>
   `${releaseInfo.url ? `[${releaseInfo.name}](${releaseInfo.url})` : `\`${releaseInfo.name}\``}`;
 
 module.exports = (issue, releases, nextRelease) => {
   return {
+    tagFormat: `${PACKAGE_NAME}@\${version}`,
     verifyConditions: [],
     verifyRelease: ['@semantic-release/changelog', '@semantic-release/npm', '@semantic-release/github']
       .map(require)
@@ -14,9 +16,13 @@ module.exports = (issue, releases, nextRelease) => {
       '@semantic-release/git'
     ],
     npmPublish: true,
-    successComment: releases.length <= 1 ? false : `:tada: This ${issue.pull_request ? 'PR is included' : 'issue has been resolved'} in version ${
-      nextRelease.version
-      } :tada:\n\nThe release is available on ${linkify(releases[1])}
-    Your **[semantic-release](${HOME_URL})** bot :package::rocket:`
+    successComment: `${
+      releases.length > 1
+        ? `:tada: This ${issue.pull_request ? 'PR is included' : 'issue has been resolved'} in version ${
+        nextRelease.version
+        } :tada:\n\nThe release is available on ${linkify(releases[0])}
+            Your **[semantic-release](${HOME_URL})** bot :package::rocket:`
+        : ''
+      }`
   }
 };
