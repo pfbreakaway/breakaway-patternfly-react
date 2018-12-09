7b8bc19b4714f38811ace84c8a4ecc3661c326bb
Joachim Schuler <jschuler@redhat.com>
2018-12-09 09:26:12 -0500

    chore: es6 function [skip ci]

--- release.config.js
+++ release.config.js
@@ -14,24 +14,24 @@ successComment: `${
  }`
  */
 
-module.exports = (issue, releases, nextRelease) => {
+module.exports = (issue, releases, nextRelease) => ({
   tagFormat: `${PACKAGE_NAME}@\${version}`,
-    verifyConditions: [],
-      verifyRelease: ['@semantic-release/changelog', '@semantic-release/npm', '@semantic-release/github']
-        .map(require)
-        .map(x => x.verifyConditions),
-        prepare: [
-          '@semantic-release/changelog',
-          '@semantic-release/npm',
-          '@semantic-release/git'
-        ],
-          npmPublish: true,
-            successComment: `${
-              releases.length > 1
-                ? `:tada: This ${issue.pull_request ? 'PR is included' : 'issue has been resolved'} in version ${
-                nextRelease.version
-                } :tada:\n\nThe release is available on ${linkify(releases[0])}
+  verifyConditions: [],
+  verifyRelease: ['@semantic-release/changelog', '@semantic-release/npm', '@semantic-release/github']
+    .map(require)
+    .map(x => x.verifyConditions),
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ],
+  npmPublish: true,
+  successComment: `${
+    releases.length > 1
+      ? `:tada: This ${issue.pull_request ? 'PR is included' : 'issue has been resolved'} in version ${
+      nextRelease.version
+      } :tada:\n\nThe release is available on ${linkify(releases[0])}
         Your **[semantic-release](${HOME_URL})** bot :package::rocket:`
-                : ''
-              }`
-};
+      : ''
+    }`
+});
