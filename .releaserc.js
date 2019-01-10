e00df938ce693697249127c06f78bf278a4c67b4
Joachim Schuler <jschuler@redhat.com>
2019-01-10 17:07:24 -0500

    tag [skip ci]

--- .releaserc.js
+++ .releaserc.js
@@ -1,8 +1,10 @@
-const hooks = require('semantic-release-monorepo-hooks')
-const output = hooks()
+const hooks = require('semantic-release-monorepo-hooks');
+const output = hooks();
+const PACKAGE_NAME = process.env.LERNA_PACKAGE_NAME || process.env.npm_package_name;
 
 module.exports = {
   branch: 'master',
+  tagFormat: `${PACKAGE_NAME}@\${version}`,
   prepare: [
     '@semantic-release/changelog',
     '@semantic-release/npm',
