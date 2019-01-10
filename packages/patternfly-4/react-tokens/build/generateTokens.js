26c519a9bfd0be63d76579e5db68500a3a69a34b
Joachim Schuler <jschuler@redhat.com>
2019-01-10 16:38:57 -0500

    fix: change 4

--- packages/patternfly-4/react-tokens/build/generateTokens.js
+++ packages/patternfly-4/react-tokens/build/generateTokens.js
@@ -1,5 +1,5 @@
 /* eslint-disable global-require,import/no-dynamic-require */
-/** change 3 */
+/** change 4 */
 const glob = require('glob');
 const { dirname, resolve, join } = require('path');
 const { parse } = require('css');
