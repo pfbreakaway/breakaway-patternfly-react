524c4db23c73831d2b66dff17cd359814a063a81
Joachim Schuler <jschuler@redhat.com>
2019-01-11 11:46:35 -0500

    fix: c11

--- packages/patternfly-4/react-core/src/components/Avatar/Avatar.js
+++ packages/patternfly-4/react-core/src/components/Avatar/Avatar.js
@@ -3,7 +3,7 @@ import styles from '@patternfly/patternfly-next/components/Avatar/avatar.css';
 import { css } from '@breakaway/react-styles';
 import PropTypes from 'prop-types';
 
-// change 10
+// change 11
 
 const propTypes = {
   /** Additional classes added to the Avatar. */
