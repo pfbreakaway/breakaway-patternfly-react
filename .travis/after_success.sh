3991cc6fbeb6fffec39af9b92b6449c5fbfcd484
Joachim Schuler <jschuler@redhat.com>
2019-01-11 11:13:40 -0500

    after success [skip ci]

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -8,6 +8,9 @@ fi
 
 if [ "${TRAVIS_REPO_SLUG}" == "${TRIGGER_REPO_SLUG}" -a "$TRAVIS_BRANCH" == "${TRIGGER_REPO_BRANCH}" ]; then
   # note: yarn travis-deploy-once "yarn lerna:publish" currently breaks NPM_TOKEN auth context
+  # export env variables declared in travis config
   export GITHUB_AUTH="${GH_TOKEN}"
-  npm run travis-deploy-once "npm run release; npm run lerna:changelog"
+  export GH_TOKEN="${GH_TOKEN}"
+  export GH_USER="${GH_USERNAME}"
+  # npm run travis-deploy-once "npm run release"
 fi
